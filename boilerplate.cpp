cf4f554571ddb385ea49ba384ef59ee5603dac97
Andrew <ivanov-andrey15@mail.ru>
2019-03-09 12:38:37 +0300

    Create boilerplate.cpp
    boilerplate for olympiads

---
+++ boilerplate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <memory.h>
+#include <cmath>
+
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <stack> 
+#include <set> 
+#include <map>
+#include <list>
+#include <bitset>
+#include <algorithm> 
+
+#define INF (2147483647)
+#define PI (3,1415926535897932384626433832795)
+
+using namespace std;
+
+typedef long long LL;
+    
+int main() {
+
+    return 0;
+}
